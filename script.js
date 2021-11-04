990b934d7f8127621298c2caa733fd633ff0ba2e
George Elioz <el1oz@Georges-MacBook-Pro.local>
2021-11-05 03:10:52 +0400

    Plus function and more

--- script.js
+++ script.js
@@ -3,42 +3,68 @@
 const monitorNum = document.querySelector('.monitor');
 
 let numbers = [];
-//======= Nikitas code
-function isAllowed(key) {
-    return ['1', '2', '3', '4', '5', '6', '0', '7', '8', '9'].includes(key);
-}
-
-function getTextContext(key) {
-    return isAllowed(key) ? pushNumbers(key) : console.log('string not allowed');
-}
-
-function pushNumbers(key) {
-    numbers.push(key);
-    monitorNum.textContent = numbers.join('');
-}
-
-document.addEventListener('keydown', (event) => {
-    getTextContext(event.key);
-})
-
-const calcNumbers = ['._0', '._1', '._2', '._3', '._4', '._5', '._6', '._7', '._8', '._9'];
-
-for (let num of calcNumbers) {
+const calculatorNumbersClass = ['._0', '._1', '._2', '._3', '._4', '._5', '._6', '._7', '._8', '._9', '._C', '._plus', '._equals', '._minus', '._multiplication', '._division', '._point'];
+// For click event
+for (let num of calculatorNumbersClass) {
     document.querySelector(num).addEventListener('click', () => {
-        getTextContext(num.replace('._', ''));
+        if (num === '._plus')
+            getTextContext('+');
+        else if (num === '._minus')
+            getTextContext('-');
+        else if (num === '._multiplication')
+            getTextContext('*');
+        else if (num === '._division')
+            getTextContext('/');
+        else if (num === '._point')
+            getTextContext('.');
+        else if (num === '._equals')
+            getTextContext('=');
+        else
+            getTextContext(num.replace('._', ''));
     })
 }
-/* ======================================== */
-const C = document.querySelector('.C');
-// Assigning empty array to reset monitor
-C.addEventListener('click', () => {
-    numbers = [];
-    monitorNum.textContent = numbers;
+// For keydown event
+document.addEventListener('keydown', event => {
+    getTextContext(event.key);
 })
-document.addEventListener('keydown', (event) => {
-    if (event.key === 'c' || event.key === 'C') {
-        numbers = [];
-        monitorNum.textContent = numbers;
+// Checking if symbol is in our allowed list
+const symbols = ['c', 'C', '+', '-', '*', '/', '=', '.'];
+const number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+const isAllowed = function (key) {
+    return symbols.includes(key) || number.includes(key);
+}
+// Pushes key to number array and prints it to monitor
+let args = [];
+const pushNumbers = function (key) {
+    if (number.includes(key)) {
+        numbers.push(key);
+        monitorNum.textContent = numbers.join('');
+    } else if (symbols.includes(key)) {
+        if (key === 'c' || key === 'C') {
+            numbers = [];
+            args = [];
+            monitorNum.textContent = '0';
+        } else if (key === '+') {
+            let arrToNumber = Number(numbers.map((toInt) => parseInt(toInt, 10)).join(''));
+            args.push(arrToNumber);
+            plus(...args);
+            numbers = [];
+        }
     }
-})
-/* ======================================== */
+    numbers.length > 15 && numbers.shift();
+}
+
+// If key is allowed pushNumbers function pushes to numbers array
+const getTextContext = function (key) {
+    return isAllowed(key) ? pushNumbers(key) : console.log('Character not allowed.');
+}
+
+const plus = function (...args) {
+    let sum = 0;
+    for (let i = 0; i < args.length; i++) {
+        sum += args[i];
+    }
+    monitorNum.textContent = sum;
+    return sum;
+}
