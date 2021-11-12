08636247645cb59a6a447ae2d056806e929d85d1
George Elioz <el1oz@Georges-MacBook-Pro.local>
2021-11-12 04:33:23 +0400

    Refactured code to more simple way

---
+++ README.md
@@ -0,0 +1,3 @@
+# Calculator
+
+I made this calculator using eval() method. I know it's not good practice, but this whole project is only practice :P
--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 'use strict';
 
-const monitorNum = document.querySelector('.monitor');
-
-let numbers = [];
-const calculatorNumbersClass = ['._0', '._1', '._2', '._3', '._4', '._5', '._6', '._7', '._8', '._9', '._C', '._plus', '._equals', '._minus', '._multiplication', '._division', '._point'];
+const calculatorNumbersClass = ['._C', '._0', '._1', '._2', '._3', '._4', '._5', '._6', '._7', '._8', '._9', '._plus', '._equals', '._minus', '._multiplication', '._division', '._point'];
 // For click event
 for (let num of calculatorNumbersClass) {
     document.querySelector(num).addEventListener('click', () => {
@@ -28,62 +25,52 @@ document.addEventListener('keydown', event => {
     getTextContext(event.key);
 })
 // Checking if symbol is in our allowed list
-const symbols = ['c', 'C', '+', '-', '*', '/', '=', '.'];
+const symbol = ['c', 'C', '+', '-', '*', '/', '=', '.', 'Enter', 'Backspace'];
 const number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 const isAllowed = function (key) {
-    return symbols.includes(key) || number.includes(key);
+    return symbol.includes(key) || number.includes(key);
 }
-// Pushes key to number array and prints it to monitor
-let args = [];
-const pushNumbers = function (key) {
-    if (number.includes(key)) {
-        numbers.push(key);
-        monitorNum.textContent = numbers.join('');
-    } else if (symbols.includes(key)) {
+// If key is allowed calculator function is called
+const getTextContext = function (key) {
+    return isAllowed(key) && calculator(key);
+}
+// Pushes key to number and prints it to monitor
+let numbers = '';
+let result = 0;
+
+const monitorNum = document.querySelector('.monitor');
+
+const calculator = function (key) {
+    // if key is a number or key is a symbol it is added to 'numbers'
+    if (number.includes(key) || symbol.includes(key)) {
+        monitorNum.textContent = numbers += key.replaceAll('Backspace', '');
+
+        if (key === 'Backspace') {
+            numbers = numbers.slice(0, numbers.length - 1);
+            monitorNum.textContent = numbers;
+        }
+        // C clears numbers and result and manipulates with DOM pointerEvents
         if (key === 'c' || key === 'C') {
-            numbers = [];
-            args = [];
-            monitorNum.textContent = '0';
-        } else if (key === '+') {
-            let arrToNumber = Number(numbers.map(toInt => parseInt(toInt, 10)).join(''));
-            args.push(arrToNumber);
-            plus(...args);
-            numbers = [];
-        } else if (key === '-') {
-            let arrToNumber = Number(numbers.map(toInt => parseInt(toInt, 10)).join(''));
-            args.push(arrToNumber);
-            minus(...args);
-            numbers = [];
+            numbers = '';
+            result = 0;
+            monitorNum.textContent = result;
+
+            for (let i = 1; i < calculatorNumbersClass.length; i++) {
+                document.querySelector(calculatorNumbersClass[i]).style.pointerEvents = 'auto';
+            }
         }
     }
-    numbers.length > 15 && numbers.shift();
+
+    if (key === '=' || key === 'Enter') {
+        numbers = numbers.replaceAll('=', '').replaceAll('Enter', '');
+        result = eval(numbers);
+        monitorNum.textContent = result;
+
+        for (let i = 1; i < calculatorNumbersClass.length; i++) {
+            document.querySelector(calculatorNumbersClass[i]).style.pointerEvents = 'none';
+        }
+        // if numbers is more than 15 symbols, first character is substracted
+        numbers.length > 15 && (numbers = numbers.substr(1));
+    }
 }
-
-// If key is allowed pushNumbers function pushes to numbers array
-const getTextContext = function (key) {
-    return isAllowed(key) ? pushNumbers(key) : console.log('Character not allowed.');
-}
-
-const plus = function (...args) {
-    let sum = 0;
-    for (let i = 0; i < args.length; i++)
-        sum += args[i];
-    monitorNum.textContent = sum;
-
-    return sum;
-}
-
-const minus = function (...args) {
-    let result = args[0];
-    for (let i = 1; i < args.length; i++)
-        result -= args[i];
-
-    console.log(result);
-    monitorNum.textContent = result;
-
-    return result;
-}
-
-
-/* FIX PLUS AND MINUS BUG */
